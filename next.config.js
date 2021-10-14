59dd5c6e92310e988852f27cffbc5b4d0ea3d6ff
Jose Roberto <jrcampos82@gmail.com>
2021-10-14 12:54:09 -0400

    fix pwd

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   
   env: {
     "BASE_URL": "http://localhost:3000",
-    "MONGODB_URL": "mongodb+srv://user-nextdb:SACyf3nn9MKfxkuL@cluster0.rmtnn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    "dbpwd": "SACyf3nn9MKfxkuL",
+    "MONGODB_URL": "mongodb+srv://user-nextdb:<pwd>@cluster0.rmtnn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    
   }
 }
