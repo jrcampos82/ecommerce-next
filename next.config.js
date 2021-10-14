2fc5fcb5a85d4cad10686d7d02401c56be135a69
Jose Roberto <jrcampos82@gmail.com>
2021-10-13 22:58:38 -0400

    commit aula 13-10

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   
   env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://user-nextdb:SACyf3nn9MKfxkuL@cluster0.rmtnn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "dbpwd": "SACyf3nn9MKfxkuL",
   }
 }
---
+++ pages/cadastro.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/Link'
+
+const cadastro = () => {
+  return (
+    <div>
+      <Head>
+        <title>Cadastro de Usuário</title>
+      </Head>
+      <form className="mx-auto my-4" style={{ maxWidth: '500px' }}>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Nome
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputNome"
+            aria-describedby="emailHelp"
+            placeholder="Digite seu nome"
+          />
+          <div id="emailHelp" className="form-text"></div>
+        </div>
+
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="email@email.com"
+          />
+          <div id="emailHelp" className="form-text"></div>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="********"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Confirme o Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword2"
+            placeholder="********"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary w-100">
+          cadastro
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default cadastro
